e8ad1b973a58e6884c38542d210a139ffad8db58
SamFelsted <61361381+SamFelsted@users.noreply.github.com>
2020-09-02 11:55:17 -0700

    Delete cfg.py

--- cfg.py
+++
@@ -1,3 +0,0 @@
-insults = ['...But what kind of a play was that? I’d rather ff this earth if I were you',
-           "...Team diff?", "...Its not your fault, your mother should have ff'ed your life", "...?",
-           "...This isn't cool math games", "...WHAT ARE YOU DOING!!!!"]
