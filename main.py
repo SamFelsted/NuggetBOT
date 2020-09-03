ad99d3c3484858b0639552ee6cc3684bba6b9cc4
SamFelsted <61361381+SamFelsted@users.noreply.github.com>
2020-09-03 07:33:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ import random
 import cfg
 
 client = discord.Client()
-# noinspection PyRedeclaration
+
 client = commands.Bot(command_prefix='$')
 flames = cfg.insults
+anime = cfg.violations
 
 
 @client.event
 async def on_ready():
     print("log-in good")
+    activity = discord.Activity(name='you all fail at league', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity)
+    print("Welcome to the NugBot Backend!")
 
 
 @client.command()
@@ -21,21 +25,41 @@ async def ping(ctx):
 
 
 @client.command()
-async def lolrate(ctx, name):
-    data = riotsearch.findStats(name)
-    won = riotsearch.winCheck(name)
-    game = 'lost'
-    if won:
-        game = "won"
-    message = f"Ah yes {name} had a kda of {data} and {game} the game..."
-    await ctx.send(message)
-    if won and data >= 1.5:
-        await ctx.send("Not bad...")
-    elif not won and data >= 3:
-        await ctx.send("Well you lost, guess you can't carry your team that hard")
+async def talk(ctx):
+    if ctx.author.id == 672959705672581161:
+        message = input('What do you want to send? ')
+        await ctx.send(message)
+        resp = input('again? y/n ')
+        if resp == 'y':
+            await talk(ctx)
+        else:
+            print("Chat ended")
     else:
-        await ctx.send(flames[random.randint(0, len(flames) - 1)])
+        print("Hey some idiot tried to use me")
 
+
+@client.command()
+async def lolrate(ctx, name):
+    activity = discord.Activity(name='you all fail at league', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity)
+    data = riotsearch.findStats(name)
+    if data != "No data":
+        won = riotsearch.winCheck(name)
+        game = 'lost'
+        if won:
+            game = "won"
+        message = f"Ah yes {name} had a kda of {data} and {game} the game..."
+        await ctx.send(message)
+        if won and data >= 1.5:
+            await ctx.send("Not bad...")
+        elif not won and data >= 3:
+            await ctx.send("Well you lost, guess you can't carry your team that hard")
+        else:
+            await ctx.send(flames[random.randint(0, len(flames) - 1)])
+            activity = discord.Activity(name=(name + ' fail'), type=discord.ActivityType.watching)
+            await client.change_presence(activity=activity)
+    else:
+        print("Not found")
+        await ctx.send(f"No Data for {name}")
 if __name__ == "__main__":
     client.run(cfg.token)
-
