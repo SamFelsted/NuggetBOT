4a407e26668d3e54cbf0907803c5661c1c05e724
SamFelsted <61361381+SamFelsted@users.noreply.github.com>
2020-09-03 07:11:43 -0700

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,8 @@
+import os
+insults = ['...But what kind of a play was that? I’d rather ff this earth if I were you',
+           "...Team diff?", "...Its not your fault, your mother should have ff'ed your life", "...?",
+           "...This isn't cool math games", "...WHAT ARE YOU DOING!!!!", "...Ur Trash lol",
+           "...How much more can you feed?", "...huh...that bad eh?"]
+apiKey = os.environ.get("KEY")
+token = os.environ.get("TOKEN")
+violations = ['heck', 'frick']
