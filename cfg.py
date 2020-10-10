2424a3f06cfd446ffa2f5d1db6e776c3c03c79b3
SamFelsted <61361381+SamFelsted@users.noreply.github.com>
2020-10-10 11:17:09 -0700

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -2,7 +2,12 @@ import os
 insults = ['...But what kind of a play was that? I’d rather ff this earth if I were you',
            "...Team diff?", "...Its not your fault, your mother should have ff'ed your life", "...?",
            "...This isn't cool math games", "...WHAT ARE YOU DOING!!!!", "...Ur Trash lol",
-           "...How much more can you feed?", "...huh...that bad eh?"]
+           "...How much more can you feed?", "...huh...that bad eh?", "...Tatical question mark",
+           "...not ez", "...commit uninstall", "...LOL", "...ggs, but not for you"
+           ]
+
+good = ['...Not bad', "... I've seen worse", "...ez", "... Yeah they were probably bots",
+        "... huh, didn't see that coming"]
 apiKey = os.environ.get("KEY")
 token = os.environ.get("TOKEN")
 violations = ['owo', 'uwu', 'o w o', 'u w u']
